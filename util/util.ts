afdc07d1a5d0680049d30f70cf474b42a17d050e
alfonsserra <alfonsoserra@gmail.com>
2018-04-27 07:22:20 +0200

    Refactor

--- util/util.ts
+++ util/util.ts
@@ -1,56 +1,62 @@
+import { browser, ElementFinder } from 'protractor';
+
 import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
 
-import {browser, ElementFinder} from 'protractor';
 let mask_fn = require('./mask').MASK_FN;
 
 /*
  * Create shim for LooksSame typing.
  */
 export interface LooksSame {
-  (img1: string, img2: string, options: Object, check: (error: any, equal: any) => void);
-  (img1: string, img2: string, check: (error: any, equal: any) => void);
-  createDiff(options: Object, err: Function);
+	(img1: string, img2: string, options: Object, check: (error: any, equal: any) => void);
+
+	(img1: string, img2: string, check: (error: any, equal: any) => void);
+
+	createDiff(options: Object, err: Function);
 }
 
 let looksSame: LooksSame = require('looks-same');
 
-export async function compareScreenshot(data, golden) {
-  let screenshotPath = await writeScreenshot(data);
-  const update = process.env['UPDATE_GOLDEN_IMAGES'] == "1" || process.env['UPDATE_GOLDEN_IMAGES'] === "true";
-  if (update) {
-    console.log('Create golden image.');
-    fs.writeFileSync(golden, fs.readFileSync(screenshotPath));
-    return true;
-  } else {
-    return new Promise<boolean>((resolve, reject) => {
-      looksSame(screenshotPath, golden, {strict: false, tolerance: 2.5}, (error, equal) => {
-        if (!equal) {
-          looksSame.createDiff({
-            reference: golden,
-            current: screenshotPath,
-            diff: 'diff.png',
-            highlightColor: '#ff00ff'}, (error) => {
-            reject(`Screenshots do not match for ${golden}.`);
-          });
-        } else {
-          resolve(true);
-        }
-      });
-    });
-  }
+export async function addMask(element: ElementFinder, color) {
+	let size = await element.getSize();
+	let location = await element.getLocation();
+	await browser.executeScript(mask_fn, location.x, location.y, size.width, size.height, color);
 }
 
-async function writeScreenshot(data) {
-  const folder = fs.mkdtempSync(`${os.tmpdir()}${path.sep}`);
-  let screenshotFile = path.join(folder, 'new.png');
-  fs.writeFileSync(screenshotFile, data, 'base64');
-  return screenshotFile;
+export async function compareScreenshot(screenShot, referenceFolderName, referenceFileName) {
+	let temporalFile = await createTemporalFile(screenShot);
+	let referenceFile = referenceFolderName + path.sep + referenceFileName;
+
+	const shouldUpdate = process.env['UPDATE_GOLDEN_IMAGES'] == "1" || process.env['UPDATE_GOLDEN_IMAGES'] === "true";
+	if (shouldUpdate) {
+		console.log('Create reference image.');
+		fs.writeFileSync(referenceFile, fs.readFileSync(temporalFile));
+		return true;
+	} else {
+		return new Promise<boolean>((resolve, reject) => {
+			looksSame(temporalFile, referenceFile, {strict: false, tolerance: 2.5}, (error, equal) => {
+				if (!equal) {
+					looksSame.createDiff({
+						reference:      referenceFile,
+						current:        temporalFile,
+						diff:           referenceFolderName + path.sep + 'diff' + path.sep + referenceFileName,
+						highlightColor: '#ff00ff'
+					}, (error) => {
+						reject(`Screenshot do not match for ${referenceFile}.`);
+					});
+				} else {
+					resolve(true);
+				}
+			});
+		});
+	}
 }
 
-export async function addMask(el: ElementFinder, color) {  
-  let size = await el.getSize();
-  let location = await el.getLocation();
-  await browser.executeScript(mask_fn, location.x, location.y, size.width, size.height, color);
+async function createTemporalFile(data) {
+	const temporalFolder = fs.mkdtempSync(`${os.tmpdir()}${path.sep}`);
+	let temporalFile = path.join(temporalFolder, 'new.png');
+	fs.writeFileSync(temporalFile, data, 'base64');
+	return temporalFile;
 }
